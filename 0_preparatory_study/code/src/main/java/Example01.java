a6ddd8e0ebb3f1611b170c78a1ed7e97d9d32245
Gilbert/Ki-Sung <kcs4912@gmail.com>
2024-06-23 23:09:52 +0900

    upload java code

---
+++ 0_preparatory_study/code/src/main/java/Example01.java
@@ -0,0 +1,15 @@
+public class Example01 {
+  public static void main(String[] args) {
+    // 화면에 내용 출력하는 예제 <-- 주석은 "//" 사용
+
+    // 내용 출력 후 줄 바뀜
+    System.out.println("Hello Java World");
+
+    // 내용만 출력하기, 줄 안 바뀜
+    System.out.print("Back-end");
+    System.out.print("Programmer");
+
+    // 내용 없이 줄바꿈만 출력
+    System.out.println();
+  }
+}
