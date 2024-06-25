7eba645b0ed4d0360287155cbad74f433a0a7d37
Gilbert/Ki-Sung <kcs4912@gmail.com>
2024-06-25 20:33:16 +0900

    Add java code for Using_Arithmetic_Operators and delete txt file

---
+++ 0_preparatory_study/code/src/main/java/Example03.java
@@ -0,0 +1,29 @@
+class Example03
+{
+  public static void main(String[] args)
+  {
+    // 변수 설정 
+    int num1 = 5;
+    int num2 = 2;
+
+    // 더하기
+    int result1 = num1 + num2;
+    System.out.println(result1);
+
+    // 빼기 
+    int result2 = num1 - num2;
+    System.out.println(result2);
+
+    // 곱하기
+    int result3 = num1 * num2;
+    System.out.println(result3);
+
+    // 나누기
+    int result41 = num1 / num2;
+    System.out.println(result41);
+
+    // 나머지
+    int result42 = num1 % num2;
+    System.out.println(result42);
+  }
+}
