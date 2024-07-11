ac50cc35203e0f7988050d934344ebe6c06c26c8
Gilbert/Ki-Sung <kcs4912@gmail.com>
2024-07-11 11:45:02 +0900

    Add code for Java - Add Data Type Integer example code

---
+++ 01_Java_Basic_Programming/code/src/Chap_01_DataType/_Ex01_VariableType.java
@@ -0,0 +1,19 @@
+package Chap_01_DataType;
+
+public class _Ex01_VariableType {
+    public static void main(String[] args) {
+        {
+            // 데이터 타입 1 정수형
+            byte num1 = 1;      // byte 타입
+            short num2 = 1;     // short 타입
+            int num3 = 1;       // int 타입
+            long num4 = 1;      // long 타입
+
+            // 결과 출력
+            System.out.println(num1);
+            System.out.println(num2);
+            System.out.println(num3);
+            System.out.println(num4);
+        }
+    }
+}
