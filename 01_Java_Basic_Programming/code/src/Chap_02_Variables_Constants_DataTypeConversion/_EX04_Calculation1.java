db54877dca4c73942d8b012b2e11f5094f8a6b28
Gilbert/Ki-Sung <kcs4912@gmail.com>
2024-07-28 23:50:21 +0900

    Add Example 4 for data typel conversion

---
+++ 01_Java_Basic_Programming/code/src/Chap_02_Variables_Constants_DataTypeConversion/_EX04_Calculation1.java
@@ -0,0 +1,15 @@
+package Chap_02_Variables_Constants_DataTypeConversion;
+
+public class _EX04_Calculation1 {
+    public static void main(String[] args) {
+        byte num11 = 1;
+        byte num12 = 2;
+        byte result1 = num11 + num12;               // 1번 - 에러 발생
+
+        short num21 = 1;
+        short num22 = 2;
+        short result21 = num21 + num22;             // 2번 - 에러 발생
+
+        short result22 = (short)(num21 + num22);    // 3번 - 강제 형변환
+    }
+}
