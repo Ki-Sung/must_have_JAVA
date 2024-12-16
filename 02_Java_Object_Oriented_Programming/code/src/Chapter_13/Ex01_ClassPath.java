04281eb107d921bc1fa9f18a806da42def2a73a1
Gilbert/Ki-Sung <kcs4912@gmail.com>
2024-12-16 13:10:13 +0900

    Add Classpath Example code

---
+++ 02_Java_Object_Oriented_Programming/code/src/Chapter_13/Ex01_ClassPath.java
@@ -0,0 +1,9 @@
+package Chapter_13;
+
+// 자바 코드 실행
+public class Ex01_ClassPath {
+    public static void main(String[] args) {     // Java 프로그램의 시작점
+        Apple apple = new Apple();               // Apple 클래스 객체 생성 - apple이라는 변수에 할당
+        apple.showName();                        // apple 객체의 showName() 메서드 호출
+    }
+}
