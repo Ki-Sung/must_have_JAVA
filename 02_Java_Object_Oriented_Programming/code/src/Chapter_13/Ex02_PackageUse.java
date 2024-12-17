0a17b844b208f0c86cecaa1eae962844d9a03cc7
Gilbert/Ki-Sung <kcs4912@gmail.com>
2024-12-17 12:50:14 +0900

    Add Example code for Package

---
+++ 02_Java_Object_Oriented_Programming/code/src/Chapter_13/Ex02_PackageUse.java
@@ -0,0 +1,15 @@
+package Chapter_13;
+
+// Banana 클래스를 Chapter_13.com.study 패키지에서 가져오기
+import Chapter_13.com.study.Banana;
+
+public class Ex02_PackageUse {
+    public static void main(String[] args) {    // Java 프로그램의 시작점
+        Apple apple = new Apple();              // Apple 클래스 객체 생성 - apple이라는 변수에 할당
+        apple.showName();                       // apple 객체의 showName() 메서드 호출
+
+        // Banana banana = new Banana();
+        Chapter_13.com.study.Banana banana = new Chapter_13.com.study.Banana(); // Banana 클래스의 객체를 생성 - anana 클래스가 Chapter_13.com.study 패키지에 정의되어 있으므로, 패키지 이름을 포함하여 객체를 생성
+        banana.showName();                      // banana 객체의 showName() 메서드 호출
+    }
+}
---
+++ 02_Java_Object_Oriented_Programming/code/src/Chapter_13/com/study/Banana.java
@@ -0,0 +1,8 @@
+package Chapter_13.com.study;
+
+// 클래스 정의
+public class Banana {                   // Banana 클래스 정의 - public 접근 제어자로 선언 되어, 다른 클래스에서도 접근 가능
+    public void showName() {            // showName() 메서드 정의 - public으로 정의되어 있음
+        System.out.println("My name is banana.");   // 메서드 호출 시 저정된 메시지 콘솔에 출력
+    }
+}
